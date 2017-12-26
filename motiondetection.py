9590cd21850892aa933da65e3be65db29b833e65
edmar <ejsgon@gmail.com>
2017-12-26 21:21:01 +0000

    Initial commit

---
+++ motiondetection.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+pirsensor = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pirsensor, GPIO.IN, GPIO.PUD_DOWN)
+
+previous_state = False
+current_state = False
+
+while True:
+   time.sleep(0.1)
+   previous_state = current_state
+   current_state = GPIO.input(pirsensor)
+   if current_state != previous_state:
+      if current_state:
+         print('Motion Detected!')
